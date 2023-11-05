28777198acdbd3f5610febb71f9662cef2619778
Aliharu <christopherdron@gmail.com>
2023-11-05 10:59:16 -0700

    version compatability update

--- scripts/module.js
+++ scripts/module.js
@@ -2,7 +2,7 @@ import { SystemManager } from './system-manager.js'
 
 Hooks.once('tokenActionHudCoreApiReady', async () => {
     const MODULE_ID = 'token-action-hud-exaltedthird';
-    const REQUIRED_CORE_MODULE_VERSION = '1.4'
+    const REQUIRED_CORE_MODULE_VERSION = '1.5'
     const module = game.modules.get(MODULE_ID);
     module.api = {
         requiredCoreModuleVersion: REQUIRED_CORE_MODULE_VERSION,
